a074dfe574999abb8721cda9a9a0e4477ae27247
chai2010 <chaishushan@gmail.com>
2015-04-09 12:39:12 +0800

    x/image/webp: 初译

--- src/golang.org/x/image/webp/doc_zh_CN.go
+++ src/golang.org/x/image/webp/doc_zh_CN.go
@@ -7,11 +7,20 @@
 // Package webp implements a decoder for WEBP images.
 //
 // WEBP is defined at: https://developers.google.com/speed/webp/docs/riff_container
+
+// webp 包实现了 WEBP 图像格式的解码器.
+//
+// WEBP 图像格式规范
+// https://developers.google.com/speed/webp/docs/riff_container
 package webp
 
 // Decode reads a WEBP image from r and returns it as an image.Image.
+
+// Decode 从 r 读取 WEBP 图像, 并返回 image.Image.
 func Decode(r io.Reader) (image.Image, error)
 
 // DecodeConfig returns the color model and dimensions of a WEBP image without
 // decoding the entire image.
+
+// DecodeConfig 返回颜色模型和图像尺寸, 但是并不解码图像像素数据.
 func DecodeConfig(r io.Reader) (image.Config, error)
