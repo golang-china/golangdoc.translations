73c36de0384b95c633f7789228adce0bcc62164c
chai2010 <chaishushan@gmail.com>
2015-04-10 09:44:05 +0800

    bzip2: 合并 并 补全遗留的翻译

--- src/compress/bzip2/doc_zh_CN.go
+++ src/compress/bzip2/doc_zh_CN.go
@@ -5,15 +5,21 @@
 // +build ingore
 
 // Package bzip2 implements bzip2 decompression.
+
+// bzip2 包实现 bzip2 的解压缩.
 package bzip2
 
 // NewReader returns an io.Reader which decompresses bzip2 data from r. If r does
 // not also implement io.ByteReader, the decompressor may read more data than
 // necessary from r.
+
+// NewReader 返回一个从 r 读取 bzip2 压缩数据并解压缩后返回给调用者的 io.Reader.
 func NewReader(r io.Reader) io.Reader
 
 // A StructuralError is returned when the bzip2 data is found to be syntactically
 // invalid.
+
+// StructuralError 表示一个错误的 bzip2 数据结构.
 type StructuralError string
 
 func (s StructuralError) Error() string
